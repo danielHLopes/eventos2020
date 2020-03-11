57a83c5228ba7c672c50358a98081d3b7072c909
danielHLopes <superaluno@VHA-LABCADL-14.ifro.local>
2020-03-10 20:38:18 -0400

    aula 4

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask import render_template
 from flask_sqlalchemy import SQLAlchemy
 #importando a conexão com o banco
-
+import os
 
 app = Flask(__name__)
 
@@ -13,6 +13,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://suporte:$uportE99@local
 #conecxão com o banco
 app.config['SQLALCHEMY_ECHO'] = True
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['UPLOAD_PATH'] = os.path.dirname(os.path.abspath(__file__)) + '/static/uploads'
 
 from app.models.tables import Usuario
 from app.models.tables import Atividade
--- app/controllers/atividades.py
+++ app/controllers/atividades.py
@@ -1,7 +1,10 @@
+import os
+import uuid
+
 from app import app
 from flask import render_template
 from app.models.tables import Atividade
-
+from flask import request
 
 #Listar atividades
 @app.route('/atividades')
@@ -10,4 +13,17 @@ def listar_atividades():
     for i in range(len(a)):
         a[i].data = a[i].data.strftime('%d/%m/%Y')
     return render_template("atividades_listar.html", atividades = a)
+
+#carregar o formulário
+@app.route('/atividades/novo')
+def form_nova_atividade():
+    tipo = 'inserir'
+    return render_template("atividades_formulario.html", tipo=tipo)
+
+#cadastrar atividade
+@app.route('/atividades/novo', methods=['POST'])
+def inserir_atividade():
+        arquivo = request.files['arquivo']
+        arquivo.filename = str(uuid.uuid4())+ os.path.split(arquivo.filename)[1]
+    return
 
